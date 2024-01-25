b559b465efa0ad377ef7a2bd3f2261617712c36d
Catalin <g.ionutcatalin@protonmail.com>
2024-01-25 12:36:38 +0200

    refactor: improve font/icons sizes

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,20 +12,24 @@ import Visa from "@/components/icons/Visa";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col bg-custom-purple-800 px-4 pb-5 pt-14 text-white md:px-12 md:py-14">
-      <div className="mx-auto flex w-full max-w-7xl flex-col">
-        <div className="md:gap-x-10 lg:my-20 lg:flex lg:justify-between">
+    <footer className="flex flex-col bg-custom-purple-800 px-4 pb-5 pt-14 text-white md:px-14 md:py-14">
+      <div className="mx-auto flex w-full max-w-screen-2xl flex-col">
+        <div className="md:gap-x-10 lg:mb-20 lg:flex lg:justify-between">
           {/* Newsletter */}
           <div>
             <div className="flex flex-col gap-y-4 pb-8">
               <Logo className="lg:h-28 lg:w-28" />
-              <p className="max-w-lg lg:my-6 lg:text-2xl">
-                Subscribe to our newsletter and receive 10% off your first order
+              <p className="max-w-lg leading-7 lg:my-6">
+                Subscribe to our newsletter for a 10% discount on your first
+                order! Be the first to access exclusive offers, product updates,
+                and more. Elevate your shopping experience with our curated
+                content. Join now and enjoy the perks of being part of our
+                community!
               </p>
             </div>
             {/* Input */}
             <div className="max-w-md lg:max-w-xl">
-              <label className="flex lg:text-xl">
+              <label className="flex">
                 <input
                   type="text"
                   placeholder="Email"
@@ -38,47 +42,47 @@ const Footer = () => {
             </div>
           </div>
           {/* Follow us */}
-          <div className="my-12 flex justify-between border-y border-custom-purple-700 py-12 lg:my-0 lg:items-center lg:justify-center lg:border-x lg:border-y-0 lg:px-12 lg:py-0">
-            <h3 className="h5-semibold lg:text-3xl">Follow Us</h3>
-            <div className="ml-12 flex gap-x-7">
+          <div className="my-12 flex justify-between border-t border-custom-purple-700 py-12 lg:my-0 lg:items-center lg:justify-center lg:border-x lg:border-y-0 lg:px-12 lg:py-0">
+            <h3 className="h5-semibold h6 h6-medium">Follow Us</h3>
+            <div className="ml-12 flex gap-x-7 lg:ml-4">
               <Link
                 href="https://www.instagram.com/hoopsy_daisy_/"
                 target="_blank"
               >
-                <Facebook className="lg:h-10 lg:w-10" />
+                <Facebook />
               </Link>
               <Link
                 href="https://www.instagram.com/hoopsy_daisy_/"
                 target="_blank"
               >
-                <Instagram className="lg:h-10 lg:w-10" />
+                <Instagram />
               </Link>
               <Link
                 href="https://www.instagram.com/hoopsy_daisy_/"
                 target="_blank"
               >
-                <Twitter className="lg:h-10 lg:w-10" />
+                <Twitter />
               </Link>
             </div>
           </div>
         </div>
 
-        <div className="flex w-full flex-col items-center gap-y-3 lg:flex-row-reverse lg:justify-between">
+        <div className="flex w-full flex-col items-center gap-y-3 border-t border-t-custom-purple-700 py-4 lg:flex-row-reverse lg:justify-between">
           {/* Payment Icons */}
-          <div className="flex gap-x-2.5">
-            <GooglePay className="lg:h-auto lg:w-16" />
-            <Maestro className="lg:h-auto lg:w-16" />
-            <MasterCard className="lg:h-auto lg:w-16" />
-            <PayPal className="lg:h-auto lg:w-16" />
-            <Visa className="lg:h-auto lg:w-16" />
+          <div className="flex gap-x-2.5 md:gap-x-6">
+            <GooglePay />
+            <Maestro />
+            <MasterCard />
+            <PayPal />
+            <Visa />
           </div>
           {/* Copyright */}
-          <p className="text-sm lg:text-lg">
+          <p className="text-sm md:text-base">
             © {new Date().getFullYear()}, Made by{" "}
             <Link
               href="https://catalin-gheorghiu.dev"
               target="_blank"
-              className="text-base text-custom-green transition-all delay-300 hover:text-green-600 lg:text-xl"
+              className="text-base text-custom-green transition-all delay-300 hover:text-green-600 md:text-base"
             >
               Catalin Gheorghiu
             </Link>
--- src/components/navigation/DesktopNav.tsx
+++ src/components/navigation/DesktopNav.tsx
@@ -26,64 +26,64 @@ const DesktopNav = ({ navigation }: DesktopNavProps) => {
   }
 
   return (
-    <nav className="relative hidden items-center justify-between border-b-2 border-b-custom-black-200 p-7 shadow-[0px_2px_8px_0px_rgba(0,0,0,0.06)] md:flex">
-      <div className="flex w-full items-center gap-x-16">
-        <Logo className="lg:h-28 lg:w-28" />
-        {navigation.length > 0 && (
-          <ul className="relative flex gap-x-7">
-            {navigation.map(({ name, handle, links }) => (
-              <li
-                key={`nav-item-${handle}`}
-                className="cursor-pointer"
-                onClick={() => handleToggleMenu(name)}
-              >
-                {/* Menu Items */}
-                {links.length > 0 ? (
-                  <div className="flex items-center lg:text-xl">
-                    <p className="mr-2">{name}</p>
-                    <Arrow
-                      className={`${
-                        isMenuVisible[name] ? "-rotate-90" : "rotate-90"
-                      } transition-transform duration-500 ease-in-out lg:h-auto lg:w-8`}
-                    />
-                  </div>
-                ) : (
-                  <>
-                    <Link href={handle} className="cursor-pointer lg:text-xl">
-                      {name}
-                    </Link>
-                  </>
-                )}
-                {/* Submenu */}
-                {links.length > 0 && (
-                  <div
-                    className={`absolute mt-2 flex flex-col overflow-hidden bg-white transition-all duration-500 ease-linear ${
-                      isMenuVisible[name]
-                        ? "h-fit rounded-lg border py-2"
-                        : "h-0 border-0 border-transparent"
-                    }`}
-                  >
-                    {/* Submenu Item */}
-                    {links.map((linkItem) => (
-                      <Link
-                        key={`link-item-${linkItem.handle}`}
-                        className="px-4 py-2 hover:bg-slate-100 lg:text-lg"
-                        href={linkItem.handle}
-                      >
-                        {linkItem.name}
-                      </Link>
-                    ))}
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-      <div className="flex gap-x-5">
-        <Search className="lg:h-auto lg:w-8" />
-        <Profile className="lg:h-auto lg:w-8" />
-        <Cart className="lg:h-auto lg:w-8" />
+    <nav className="relative hidden items-center justify-between border-b-2 border-b-custom-black-200 p-7 shadow-[0px_2px_8px_0px_rgba(0,0,0,0.06)] md:flex lg:px-14">
+      <div className="mx-auto flex w-full max-w-screen-xl lg:max-w-screen-2xl">
+        <div className="flex w-full items-center gap-x-16">
+          <Logo className="lg:h-28 lg:w-28" />
+          {navigation.length > 0 && (
+            <ul className="relative flex gap-x-7">
+              {navigation.map(({ name, handle, links }) => (
+                <li
+                  key={`nav-item-${handle}`}
+                  className="flex cursor-pointer items-center"
+                  onClick={() => handleToggleMenu(name)}
+                >
+                  {/* Menu Items */}
+                  {links.length > 0 ? (
+                    <div className="flex items-center">
+                      <p className="mr-2">{name}</p>
+                      <Arrow
+                        className={`${
+                          isMenuVisible[name] ? "-rotate-90" : "rotate-90"
+                        } transition-transform duration-500 ease-in-out lg:h-auto lg:w-8`}
+                      />
+                    </div>
+                  ) : (
+                    <>
+                      <Link href={handle}>{name}</Link>
+                    </>
+                  )}
+                  {/* Submenu */}
+                  {links.length > 0 && (
+                    <div
+                      className={`absolute top-6 mt-2 flex flex-col overflow-hidden bg-white transition-all duration-500 ease-linear ${
+                        isMenuVisible[name]
+                          ? "h-fit rounded-lg border py-2"
+                          : "h-0 border-0 border-transparent"
+                      }`}
+                    >
+                      {/* Submenu Item */}
+                      {links.map((linkItem) => (
+                        <Link
+                          key={`link-item-${linkItem.handle}`}
+                          className="px-4 py-2 hover:bg-slate-100 lg:text-lg"
+                          href={linkItem.handle}
+                        >
+                          {linkItem.name}
+                        </Link>
+                      ))}
+                    </div>
+                  )}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="flex items-center gap-x-5">
+          <Search />
+          <Profile />
+          <Cart />
+        </div>
       </div>
     </nav>
   );
